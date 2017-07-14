596b3f498f6aee1f82f765942fc393862bf883c2
Manishkumar21 <khiladimanu1@gmail.com>
2017-07-14 10:59:29 +0530

    Changes applied in models.py

--- instagram/demo_app/models.py
+++ instagram/demo_app/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 255)
+    phone = models.CharField(max_length = 20)
+    age = models.IntegerField(default = 0)
+    created_on = models.DateTimeField(auto_now_add = True)
