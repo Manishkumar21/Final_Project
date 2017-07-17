74470947895d863eabd16dd0828685a53c3df8aa
Manishkumar21 <khiladimanu1@gmail.com>
2017-07-17 12:28:41 +0530

    django page is Created

--- instagram/Instagram/views.py
+++ instagram/Instagram/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from datetime import datetime
 # Create your views here.
 
 def signup_view(request):
     #Business Logic
-    return render(request,'signup.html')
+    if request.method == 'GET':
+        today = datetime.now()
+        return render(request,'signup.html',{'today':today})
--- instagram/demo_app/models.py
+++ instagram/demo_app/models.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class UserModel(models.Model):
 	email = models.EmailField()
-    name = models.CharField(max_length=120)
+	name = models.CharField(max_length=250)
 	username = models.CharField(max_length=120)
 	password = models.CharField(max_length=40)
-    created_on = models.DateTimeField(auto_now_add = True)
+	created_on = models.DateTimeField(auto_now_add = True)
 	updated_on = models.DateTimeField(auto_now = True)
